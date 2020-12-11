5746a9a95fa00df6fbffdea8071cf0c733e31fde
kyoungd <kyoungd@hotmail.com>
2020-12-11 15:29:03 -0800

    first release

--- src/component/Form.js
+++ src/component/Form.js
@@ -1,56 +1,115 @@
 import React from "react";
 import emailjs from "emailjs-com";
+import axios from 'axios';
 
 class Form extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      errors: {
-        name: "",
-        subject: "",
-        phone: "",
-        email: "",
+  state = {
+    errors: {
+      name: "",
+      phone: "",
+      email: "",
+      message: "",
+    },
+    data: {
+      name: "",
+      phone: "",
+      email: "",
+      message: "",
+    },
+    API_URL: "http://192.168.0.17:1337",
+  };
+
+  login = async () => {
+    const username = "courage";
+    const password = "password";
+    const data = { identifier: username, password };
+    console.log('login body data: ',data);
+
+    try {
+      const result = await fetch(this.state.API_URL + '/auth/local', {
+          method: 'POST',
+          headers: {
+            "Content-Type": "application/json" 
+          },
+          body:JSON.stringify(data)
+        });
+      const response = await result.json();
+      return response.jwt;
+    }
+    catch (err) {
+      console.log(err);
+      return null;
+    };
+  }
+
+  callApi = async (httpMethod, accessToken, data) => {
+    const hasData = data === undefined ? false : true;
+    const config = {
+      timeout: 3000,
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
       },
     };
+    // console.log(config)
+    const callUrl = this.state.API_URL + '/subscribers';
+    console.log(callUrl);
+    const response = await httpMethod(
+      callUrl,
+      hasData ? data : config,
+      hasData ? config : undefined
+    );
+    return response;
   }
 
   handleChange = (event) => {
     let name = event.target.name;
     let value = event.target.value;
-    let errors = this.state.errors;
+    const errors = this.state.errors;
+    const data = this.state.data;
 
     switch (name) {
       case "name":
         errors.name = value.length === 0 ? "Name is not empty" : "";
         break;
-      case "subject":
-        errors.subject = value.length < 5 ? "Subject must be 5 characters" : "";
-        break;
       case "phone":
         errors.phone = value.length < 5 ? "phone is not empty" : "";
         break;
       case "email":
-        errors.email = value.length < 5 ? "Subject is not empty" : "";
+        errors.email = value.length < 5 ? "email is not empty" : "";
+        this.setState({ data: { ...this.state.data, "email": value } });
         let appos = value.indexOf("@");
         let dots = value.lastIndexOf(".");
 
         if (appos < 1 || dots - appos < 2) {
           errors.email = "please enter valid email";
         }
-
         break;
-
       default:
         break;
     }
-    this.setState({ errors, [name]: value });
+    this.setState({ ...errors, [name]: value });
+        data[name] = value;
+        this.setState( { data } );
   };
 
   submitHandler = (e) => {
     e.preventDefault();
+
+    this.login().then(token => {
+      console.log(token)
+      this.callApi(axios.post, token, this.state.data).then(() => {
+        alert("subscription added");
+      })
+      .then(result => {
+        console.log(result);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+    });
+
     // if (
     //   this.state.errors.name.length === 0 &&
-    //   this.state.errors.subject.length === 0 &&
     //   this.state.errors.phone.length === 0 &&
     //   this.state.errors.email.length === 0
     // ) {
@@ -103,17 +162,6 @@ class Form extends React.Component {
               onChange={this.handleChange}
             />
             <p>{errors.email}</p>
-          </div>
-          <div className="col-lg-6">
-            <input
-              type="text"
-              id="subject"
-              name="subject"
-              className="form-control"
-              placeholder="Subject*"
-              onChange={this.handleChange}
-            />
-            <p>{errors.subject}</p>
           </div>
           <div className="col-lg-6">
             <input
